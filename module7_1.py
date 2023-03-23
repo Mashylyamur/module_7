463524bc180f596f25a6e40ba5131d93a3b56fe0
Mashylyamur <krabovna3@gmail.com>
2023-03-23 18:19:57 +0300

    some commit

--- module7_1.py
+++ module7_1.py
@@ -5,6 +5,5 @@ soup = BeautifulSoup(page.text, 'html.parser')
 curses = soup.find('table', {'class': 'mfd-table mfd-currency-table'}).find_all('tr')
 
 for curs in curses:
-    print(f'{curs.get_text(strip=True,separator=",")}')
-
+    print(f'{curs.get_text(strip=True, separator=" ")}')
 
--- module7_2.py
+++ module7_2.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 page = requests.get(' http://mfd.ru/currency/?currency=USD')
 soup = BeautifulSoup(page.text, 'html.parser')
 curses = soup.find('table', {'class': 'mfd-table mfd-currency-table'}).find_all('tr')
-data = [curs.get_text(strip=True, separator=",") for curs in curses]
+data = [curs.get_text(strip=True, separator=" ") for curs in curses]
+x = [str(data[2:11])]
+s = data[13:19]
+y = [float(i) for i in s]
 
-xs = []
-ys = []
-
-plt.plot(xs, ys)
+plt.plot(x, y)
 plt.grid()
 plt.show()
 
